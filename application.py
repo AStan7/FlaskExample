c69c85dee67477b1d437d11b2b82fdced039392b
AStan7 <55993416+AStan7@users.noreply.github.com>
2023-01-30 15:02:05 +0000

    (test)

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, url_for, render_template, request
 application = Flask(__name__)
 
-@application.route("/")
+@application.route("/", methods=['GET', "POST"])
 def homepage():
     return render_template("homepage.html")
