c0a97f60f8c2a3b2222a47824f11980c7332eebb
AStan7 <55993416+AStan7@users.noreply.github.com>
2023-01-30 14:39:15 +0000

    (test)

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 application = Flask(__name__)
 
 @application.route("/")
-def hello():
-    return "Hello World!"
+def homepage():
+    return render_template("homepage.html")
